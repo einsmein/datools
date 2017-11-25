5ab81836838f5bbbcdffe158a2d2a928beca0763
Michael <micke.green@gmail.com>
2017-11-25 22:08:58 +0100

    Added more functions for replacing non finite values with other finite values.

--- R/valuefunctions.R
+++ R/valuefunctions.R
@@ -33,3 +33,22 @@ naToVal <- function (x, y=0)
   x[is.na(x)] <- y
   return(x)
 }
+
+#' Replace non finite values with a user given value in a vector
+#'
+#' This function replaces all non finite values in a vector with a value provided
+#' by the user. NA, NaN, Inf are all examples of non finite values.
+#'
+#' @param x the vector to replace all non finite values in
+#' @param y the value to replace the non finite values with
+#'
+#' @return a new vector with all non finite values replaced by y
+#' @export
+#'
+#' @examples
+#' nonFinToVal(c(NA, 3:5, NA, NA, 4, 4, 10, NA))
+#' nonFinToVal(c(NA, 3:5, NA, NA, 4, 4, 10, NA), 3)
+nonFinToVal<-function(x, y=0){
+  x[!is.finite(x)]<-y
+  return(x)
+}
