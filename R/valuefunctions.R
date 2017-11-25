48ac3ae9d76b5edd690521828c1fbe92bfd41bdc
Michael <micke.green@gmail.com>
2017-11-25 21:01:36 +0100

    Added new function to replace NA with given values.

--- R/valuefunctions.R
+++ R/valuefunctions.R
@@ -14,3 +14,22 @@ naToZero <- function (x)
   x[is.na(x)] <- 0
   return(x)
 }
+
+#' Replace NA with a user given value in a vector
+#'
+#' This function replaces all NA's in a vector with a value provided by the user.
+#'
+#' @param x the vector to replace all NA in
+#' @param y the value to replace the NA with
+#'
+#' @return a new vector with all NA's replaced by y
+#' @export
+#'
+#' @examples
+#' naToZero(c(NA, 3:5, NA, NA, 4, 4, 10, NA))
+#' naToVal(c(NA, 3:5, NA, NA, 4, 4, 10, NA), 0)
+naToVal <- function (x, y=0)
+{
+  x[is.na(x)] <- y
+  return(x)
+}
