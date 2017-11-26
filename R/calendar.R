55f718276f2d4d54fdb2150df33051a7d24b2d86
Michael <micke.green@gmail.com>
2017-11-26 01:04:21 +0100

    Updated the genHolidays function to filter on countries too.

--- R/calendar.R
+++ R/calendar.R
@@ -18,6 +18,8 @@ dateToIsoWeek <- function (dates) gsub("-W|-\\d", "", ISOweek::date2ISOweek(date
 #'
 #' @param years the years for which to generate the holidays given as 2013:2017
 #' as an example where the default is the last three years
+#' @param country specifies which specific country to focus on except the "global"
+#' holidays where the possible values are "CA", "CH", "DE", "FR", "GB", "IT", "JP", "US"
 #'
 #' @return a one hot encoded tibble where each column is a holiday and the first
 #' column is the ISO date
@@ -26,12 +28,12 @@ dateToIsoWeek <- function (dates) gsub("-W|-\\d", "", ISOweek::date2ISOweek(date
 #' @examples
 #' b<-genHolidays(2016:2017)
 #' b[1:10, 1:5]
-genHolidays<-function(years=(lubridate::year(Sys.Date())-2):lubridate::year(Sys.Date())) {
+genHolidays<-function(years=(lubridate::year(Sys.Date())-2):lubridate::year(Sys.Date()), country) {
   # a<-sapply(timeDate::listHolidays(), FUN=function(x) as.character(as.Date(eval(call(x, years)))))
   a<-sapply(timeDate::listHolidays(), FUN=function(x) as.character(as.Date(eval(as.call(list(utils::getFromNamespace(x, "timeDate"), years))))))
   tmpdf <- tibble(Date=as.character(seq(from = as.Date(paste0(min(years), "-01-01")),
-                                            to = as.Date(paste0(max(years), "-12-31")),
-                                            by = "1 day")))
+                                        to = as.Date(paste0(max(years), "-12-31")),
+                                        by = "1 day")))
   toInd <- function(x){
     dplyr::left_join(tmpdf, tibble(Date=x, Val=1))
   }
@@ -39,5 +41,8 @@ genHolidays<-function(years=(lubridate::year(Sys.Date())-2):lubridate::year(Sys.
   b<-lapply(seq_along(a), FUN = function(x) toInd(a[[x]]) %>% stats::setNames(c("Date", names(a)[x])))
   b<-Reduce(dplyr::inner_join, b)
   b<-dplyr::mutate_all(b, naToZero)
+  countries<-c("CA", "CH", "DE", "FR", "GB", "IT", "JP", "US")
+  if(missing(country)) b <- dplyr::select(b, -dplyr::matches(paste0(countries, collapse = '|')))
+  else b <- dplyr::select(b, -dplyr::matches(paste0(setdiff(countries, country), collapse = '|')))
   b
 }
