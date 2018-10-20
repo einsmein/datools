004754fb8af3bb4ce84b0d82d5e857b72c5208b5
Michael Green <micke.green@gmail.com>
2018-10-20 23:47:46 +0200

    Added grammian angular field transformations to the mix. :)

--- R/timeseries.R
+++ R/timeseries.R
@@ -29,3 +29,30 @@ find_lag<-function(x,y, lag.max=as.integer(length(x)/2)) {
   res <- stats::ccf(x, y, lag.max = lag.max, na.action = stats::na.omit, plot = FALSE)
   res$lag[which.max(abs(res$acf))]
 }
+
+#' Create a Grammian Angular Field representation of a time series
+#'
+#' This function takes a univariate timeseries and turns it into a Grammian
+#' Angular Field which is a 2 dimensional representation of that time series.
+#' Typically this could be used by a convulutional neural network to infer
+#' properties of the problem and use it in predictive purposes.
+#'
+#' @param x the univariate timeseries to convert
+#'
+#' @return a matrix representing the Grammian Angular Field
+#' @export
+#'
+#' @examples
+#' library(datools)
+#' x <- sin(1:100)
+#' xg <- grammify(x)
+#' image(xg)
+#'
+#' image(grammify(cos(1:100)))
+#'
+#' x <- (seq(-100, 100, length.out=100)/10)^2+rnorm(100, 0, 10)
+#' image(grammify(x))
+grammify <- function(x) {
+  theta <- acos(standardize(x, -1, 1))
+  outer(theta, theta, FUN=function(x, y) cos(x+y))
+}
