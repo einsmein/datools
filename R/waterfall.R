fff43f818b6347113a8b9a857663cf3cb286b02a
Michael Green <micke.green@gmail.com>
2019-08-22 06:57:44 +0200

    Regenerated documentation.

--- R/waterfall.R
+++ R/waterfall.R
@@ -1,10 +1,13 @@
 #' Make waterfall data tibble from a tibble of cash flows
 #'
+#' The only requirement for this function to work is that the first row and last row of the tibble
+#' represent the cash in and cash out. In other words the Base and the Total when talking about
+#' sales modeling.
 #'
 #' @param data the data tibble to operate on
 #'
 #' @return a new data tibble with columns necessary to create a waterfall visualization
-#' @importFrom stats lag
+#' @importFrom dplyr lag mutate transmute
 #' @export
 #'
 #' @examples
@@ -24,15 +27,20 @@
 #'   amount = c(1200, -2300, 11000, -9000, -1900, -1200, -3000, 7000, 1800)
 #' )
 #' to_waterfall(datadf)
+#' # Change order
+#' datadf2 <- to_waterfall(datadf)
+#' firstindex <- which(datadf$desc == "sales")
+#' datadf2[c(firstindex, setdiff(seq_along(datadf$amount), firstindex) ),]
 to_waterfall <- function (data) {
-  desc<-amoun<-end<-id<-type<-start<-NULL
+  desc<-amount<-end<-id<-type<-start<-NULL
   datadf <- data %>%
     mutate(
       desc = factor(desc, levels=desc),
+      # desc = as.factor(desc),
       id = seq_along(amount),
       type = ifelse(amount > 0, 'in', 'out'),
       end = cumsum(amount),
-      start = stats::lag(end)
+      start = dplyr::lag(end)
     )
   datadf[c(1, nrow(datadf)), "type"] <- 'net'
   datadf[1, "start"] <- 0
@@ -44,7 +52,12 @@ to_waterfall <- function (data) {
 
 #' Make a waterfall plot from a tibble of cash flows
 #'
+#' The only requirement for this function to work is that the first row and last row of the tibble
+#' represent the cash in and cash out. In other words the Base and the Total when talking about
+#' sales modeling.
+#'
 #' @param data the data tibble to operate on
+#' @importFrom ggplot2 aes element_text geom_rect theme theme_minimal xlab ylab
 #'
 #' @return a ggplot of the waterfall plot
 #' @export
@@ -67,6 +80,7 @@ to_waterfall <- function (data) {
 #' )
 #' plot_waterfall(datadf)
 plot_waterfall <- function(data) {
+  desc<-amount<-end<-id<-type<-start<-NULL
   to_waterfall(data) %>% ggplot(aes(desc, fill = type)) +
     geom_rect(aes(
       x = desc,
@@ -78,3 +92,4 @@ plot_waterfall <- function(data) {
     xlab("") + ylab("Value") +
     theme(axis.text.x = element_text(angle = 45, vjust = 0.9, hjust=1))
 }
+#
